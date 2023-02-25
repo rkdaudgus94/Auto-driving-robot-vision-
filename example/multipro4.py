96fa60bc89ba971b4b0e2f5844608435afa8781c
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-02-25 17:31:43 +0900

    Update multipro4.py

--- example/multipro4.py
+++ example/multipro4.py
@@ -2,6 +2,33 @@ import cv2
 import multiprocessing as mp
 import pytesseract 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 def text_recognition(frame):
     # 이미지에서 텍스트를 추출합니다.
         text = pytesseract.image_to_string(frame, lang = 'kor')
@@ -11,7 +38,7 @@ def video_capture():
     cv2.cuda.setDevice(0)
     stream = cv2.cuda_Stream()
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method = 0), cv2.CAP_GSTREAMER)
     while True:
         ret, frame = cap.read()
         if not ret:
