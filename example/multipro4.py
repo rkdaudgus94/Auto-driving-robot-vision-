9bf75abad10d60a69998ec0dcb2e1ae6341e24d9
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-03-25 12:21:59 +0900

    .

--- example/multipro4.py
+++ example/multipro4.py
@@ -1,77 +1,53 @@
 import cv2
-import multiprocessing as mp
-import pytesseract 
-pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
-def gstreamer_pipeline(
-    sensor_id=0,
-    capture_width=1920,
-    capture_height=1080,
-    display_width=960,
-    display_height=540,
-    framerate=30,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc sensor-id=%d !"
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
+import dlib
+import speech_recognition as sr
+from multiprocessing import Process
 
-def text_recognition(frame):
-    # 이미지에서 텍스트를 추출합니다.
-        text = pytesseract.image_to_string(frame, lang = 'kor')
-        print(text)
+def face_detection_function():
+    cap = cv2.VideoCapture(0)
 
-def video_capture():
-    cv2.cuda.setDevice(0)
-    stream = cv2.cuda_Stream()
+    detector = dlib.get_frontal_face_detector()
 
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method = 0), cv2.CAP_GSTREAMER)
     while True:
         ret, frame = cap.read()
-        if not ret:
-            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # CPU에서 GPU로 이미지 전송
-        img = cv2.cuda_GpuMat(frame, stream=stream)
+        faces = detector(gray)
 
-    # GPU에서 색상 변환 수행
-        img = cv2.cuda.cvtColor(img, cv2.COLOR_BGR2GRAY, dst=None, stream=stream)
+        for face in faces:
+            x, y, w, h = face.left(), face.top(), face.width(), face.height()
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # GPU에서 CPU로 이미지 전송
-        frame = img.download(stream=stream)
+        cv2.imshow('Face Detection', frame)
 
-    # 이미지 출력
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    # 텍스트인식을 처리할 프로세스 생성
-    p1 = mp.Process(target=text_recognition)
+def voice_recognition_function():
+    recognizer = sr.Recognizer()
 
-    # 비디오 캡처를 처리할 프로세스 생성
-    p2 = mp.Process(target=video_capture)
+    with sr.Microphone() as source:
+        print("음성 인식을 시작합니다. 말하세요!")
+        audio = recognizer.listen(source)
 
-    # 프로세스 실행
-    p1.start()
-    p2.start()
+    try:
+        print("음성 인식 중...")
+        recognized_text = recognizer.recognize_google(audio, language="ko-KR")
+        print("인식된 텍스트: " + recognized_text)
+    except sr.UnknownValueError:
+        print("음성 인식에 실패했습니다.")
+    except sr.RequestError as e:
+        print(f"구글 음성 인식 서비스에 요청하는 동안 오류 발생; {e}")
 
-    # 프로세스 종료 대기
-    p1.join()
-    p2.join()
+if __name__ == "__main__":
+    face_detection_process = Process(target=face_detection_function)
+    voice_recognition_process = Process(target=voice_recognition_function)
+
+    face_detection_process.start()
+    voice_recognition_process.start()
+
+    face_detection_process.join()
+    voice_recognition_process.join()
