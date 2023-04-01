9e80350fc163dff9de89fb6aa82e34eb1f92b582
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-04-01 22:23:41 +0900

    Update main3.py

--- face_voice/main3.py
+++ face_voice/main3.py
@@ -7,7 +7,7 @@ def face_recognition_process():
     fr_instance.video()
 
 def speech_recognition_process():
-    speech_main()
+    r_name_list = get_r_name_list()
 
 if __name__ == "__main__":
     face_process = multiprocessing.Process(target=face_recognition_process)
