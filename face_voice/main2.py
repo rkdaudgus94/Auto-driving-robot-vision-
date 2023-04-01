33c077f73c8bc07d20ddec18f30f101bc0c23399
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-04-01 13:21:34 +0900

    Update main2.py

--- face_voice/main2.py
+++ face_voice/main2.py
@@ -4,8 +4,8 @@ from multi_face import Facerecognition
 from multi_voice import get_r_name_list
 
 def main() :
-    p0 = Process(target = mu_fa)
-    p1 = Process(target = mu_vo)
+    p0 = Process(target = mu_vo)
+    p1 = Process(target = mu_fa)
 
     p0.start()
     p1.start()
