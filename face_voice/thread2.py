504f86730155fe61e840b44cb51bc2d2919d7c38
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-04-04 19:05:26 +0900

    Update thread2.py

--- face_voice/thread2.py
+++ face_voice/thread2.py
@@ -7,22 +7,29 @@ from multi_face2 import Facerecognition
 # 공유 변수를 위한 스레드 락
 lock = threading.Lock()
 
-# 결과를 저장할 공유 변수
-shared_results = {"func1": None, "func2": None}
+def compare_values(value1, value2):
+    if value1 is not None and value2 is not None:
+        if value1 == value2:
+            print("The values match!")
+        else:
+            print("The values do not match!")
 
 def func1(add):
-        face_recognition = Facerecognition()
-        time1 = 0
-        for names in face_recognition.video():
-            if time1 == 15:
-                time1 += 1
-                with lock:
-                    shared_results["func1"] = names
+    face_recognition = Facerecognition()
+    time1 = 0
+    for names in face_recognition.video():
+        if time1 == 15:
+            time1 += 1
+            with lock:
+                compare_values(names, r_name_list)
 
 def func2(add):
+    global r_name_list
+    while True:
         r_name_list = get_r_name_list()
         with lock:
-            shared_results["func2"] = r_name_list
+            compare_values(r_name_list, r_name_list)
+        time.sleep(1)
 
 def main():
     # 스레드 정의
@@ -33,17 +40,8 @@ def main():
     thread1.start()
     thread2.start()
 
-    while True:
-        # 공유 변수를 사용하여 값이 일치하는지 확인
-        with lock:
-            if shared_results["func1"] is not None and shared_results["func2"] is not None:
-                if shared_results["func1"] == shared_results["func2"]:
-                    print("The values match!")
-                else:
-                    print("The values do not match!")
-
-        # 필요한 경우 검사 간격을 조절할 수 있습니다.
-        time.sleep(1)
+    thread1.join()
+    thread2.join()
 
 if __name__ == "__main__":
     main()
