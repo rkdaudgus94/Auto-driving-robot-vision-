281d59ae08acd4d2da123080b26a2d886f150a34
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-04-06 18:08:07 +0900

    Update thread3.py

--- face_voice/thread3.py
+++ face_voice/thread3.py
@@ -5,9 +5,11 @@ import os
 from multi_face2 import Facerecognition
 
 lock = threading.Lock() # 공유 변수
+shared_r_name_list = None
 
 # 스레드 테스트를 위해 def 2개 생성
 def func1(add):
+    global shared_r_name_list
     face_recognition = Facerecognition()
     time1 = 0
     for names in face_recognition.video():
@@ -15,7 +17,7 @@ def func1(add):
             with lock:
                 if names == shared_r_name_list:
                     print("일치합니다")
-                    
+
         elif (time1 % 11 == 0) :
             print(names)
         time1 += 1
