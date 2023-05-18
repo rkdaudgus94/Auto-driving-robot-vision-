ef5e6cd963ae46cb5fab13ac5cdb23f173b86d3a
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-05-18 15:42:17 +0900

    .

--- text/ezocr.py
+++ text/ezocr.py
@@ -1,26 +1,34 @@
 import easyocr
 import cv2
 
-def gst_pipeline(
-    sensor_id = 0,
-    capture_width = 1920,
-    capture_height = 1080,
-    display_width = 960,
-    display_height = 540,
-    framerate = 30,
-    flip_method = 0,
-) :
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
     return (
-        sensor_id,
-        capture_width,
-        capture_height,
-        framerate,
-        flip_method,
-        display_width,
-        display_height,
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
     )
 
-cap = cv2.VideoCapture(gst_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 reader = easyocr.Reader(['ko','en'], gpu=False)
 
 if cap.isOpened() :
