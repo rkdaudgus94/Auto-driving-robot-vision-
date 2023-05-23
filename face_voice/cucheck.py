f2815eb141463c988d9b315b37f838bc28a4acef
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-05-23 19:35:58 +0900

    Update cucheck.py

--- face_voice/cucheck.py
+++ face_voice/cucheck.py
@@ -1,4 +1,4 @@
-import cv2
+""" import cv2
 
 print('Is OpenCV built with CUDA:', cv2.ocl.haveOpenCL())
 
@@ -7,3 +7,15 @@ if cv2.ocl.haveOpenCL():
     print('Using CUDA.')
 else:
     print('Not using CUDA.')
+ """
+
+import cv2
+
+# Get OpenCV build information
+build_info = cv2.getBuildInformation()
+
+# Check if the build information includes CUDA support
+if 'CUDA' in build_info:
+    print('OpenCV is built with CUDA support.')
+else:
+    print('OpenCV is not built with CUDA support.')
