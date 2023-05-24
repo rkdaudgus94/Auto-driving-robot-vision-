e1cf130c3479f9618b2298f3a184d165e4843df2
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-05-24 09:44:26 +0900

    Create cameracheck.py

---
+++ face_voice/cameracheck.py
@@ -0,0 +1,42 @@
+import cv2
+
+def gstreamer_pipeline(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=820,
+    display_height=616,
+    framerate=21,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
+if cap.isOpened():
+    while True:
+        ret_val, img = cap.read()
+        cv2.imshow('camera', img)
+        if cv2.waitKey(1) == 27:  # if ESC key is pressed, then exit
+            break
+else:
+    print("Unable to open camera")
+
+cap.release()
+cv2.destroyAllWindows()
