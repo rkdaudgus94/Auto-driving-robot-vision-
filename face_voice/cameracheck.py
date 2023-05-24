d09e6a548d5fea6fcc540bfa0067ae476190fd85
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-05-24 18:51:54 +0900

    Update cameracheck.py

--- face_voice/cameracheck.py
+++ face_voice/cameracheck.py
@@ -1,23 +1,23 @@
 import cv2
 
 def gstreamer_pipeline(
-    capture_width=3280,
-    capture_height=2464,
-    display_width=820,
-    display_height=616,
-    framerate=21,
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
     flip_method=0,
 ):
     return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
         "video/x-raw, format=(string)BGR ! appsink"
         % (
+            sensor_id,
             capture_width,
             capture_height,
             framerate,
