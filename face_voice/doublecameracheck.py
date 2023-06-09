af26493cc5e949b3a730e7c7c7fd840856ee8a26
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-06-10 01:08:52 +0900

    Update doublecameracheck.py

--- face_voice/doublecameracheck.py
+++ face_voice/doublecameracheck.py
@@ -1,4 +1,5 @@
 import cv2
+import threading
 
 def gstreamer_pipeline(sensor_id=0):
     return (
@@ -9,21 +10,43 @@ def gstreamer_pipeline(sensor_id=0):
         "videoconvert ! "
         "video/x-raw, format=(string)BGR ! appsink"
     )
+def camera0() :
 
-# 카메라 0
-cap0 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=0), cv2.CAP_GSTREAMER)
-# 카메라 1
-cap1 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=1), cv2.CAP_GSTREAMER)
+    # 카메라 0
+    cap0 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=0), cv2.CAP_GSTREAMER)
+    while True :
+        ret0, frame0 = cap0.read()
+        if ret0 :
+            cv2.imshow('Camera 0', frame0)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-while True:
-    ret0, frame0 = cap0.read()
-    ret1, frame1 = cap1.read()
-    if ret0 and ret1:
-        cv2.imshow('Camera 0', frame0)
-        cv2.imshow('Camera 1', frame1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cap0.release()
+        cv2.destroyAllWindows()    
+def camera1() :
 
-cap0.release()
-cap1.release()
-cv2.destroyAllWindows()
+    # 카메라 1
+    cap1 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=1), cv2.CAP_GSTREAMER)
+    while True :
+        ret1, frame1 = cap1.read()
+        if ret1 :
+            cv2.imshow('Camera 1', frame1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap1.release()
+        cv2.destroyAllWindows()
+
+def main():
+    #스레드 정의
+    thread1 = threading.Thread(target=camera0)
+    thread2 = threading.Thread(target=camera1)
+
+    #스레드 시작
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+if __name__ == "__main__":
+    main()
