7e191bfffac810f44138cca28a5d16173d88c6c5
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-06-10 01:59:57 +0900

    .

--- face_voice/multi_face0.py
+++ face_voice/multi_face0.py
@@ -12,10 +12,10 @@ image_path = r'/home/hyeun/face_img/*.png'
 
 def gstreamer_pipeline(
     sensor_id=0,
-    capture_width=1280,
-    capture_height=720,
-    display_width=640,
-    display_height=360,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
     framerate=30,
     flip_method=0,
 ):
